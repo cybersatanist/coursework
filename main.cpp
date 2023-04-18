8fb69549c08ff156369c358c64dd7e7c0438d914
cybersatanist <130359344+cybersatanist@users.noreply.github.com>
2023-04-18 11:17:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -35,32 +35,76 @@ struct Data
 
 class Student {
 private:
-	struct Data record;
+	struct Data record[31];
 	struct Activity activity;
 	struct Mark mark;
 public:
     Student() 
     {
-        strcpy(record.surname, "Libov");
-        strcpy(record.name, "Maxim");
-        strcpy(record.patronymic, "Ivanovich");
-        record.date = 1;
-        record.month = 1;
-        record.year = 2000;
-        record.yearOfAdmission = 0;
-        strcpy(record.faculty, "IKB");
-        strcpy(record.department, "KB-1");
-        strcpy(record.group, "BBBO-06-22");
-        strcpy(record.gender, "M");
-        for (int i = 0; i < 11; i++) {
-        	strcpy(record.activity.subject[i].value, "None");
-        	record.activity.mark[i].value = 0;
+        strcpy(record[1].surname, "Surname0");
+        strcpy(record[1].name, "Name0");
+        strcpy(record[1].patronymic, "Patronymic0");
+        record[1].date = 1;
+        record[1].month = 2;
+        record[1].year = 3456;
+        record[1].yearOfAdmission = 7890;
+        strcpy(record[1].faculty, "faculty0");
+        strcpy(record[1].department, "department0");
+        strcpy(record[1].group, "group0");
+        strcpy(record[1].gender, "g");
+        for (int i = 1; i < 11; i++) {
+        	strcpy(record[1].activity.subject[i].value, "None");
+        	record[1].activity.mark[i].value = 0;
         }
     }
     ~Student() {}
+
+    void set_data(int k)
+    {
+    	cout << "\n" << "Student " << k << endl;
+    	cout.width(18); cout << "Surname: "; cin >> record[k].surname;
+    	cout.width(18); cout << "Name: "; cin >> record[k].name;
+    	cout.width(18); cout << "Patronymic: "; cin >> record[k].patronymic;
+    	cout.width(18); cout << "Date: "; cin >> record[k].date;
+    	cout.width(18); cout << "Month: "; cin >> record[k].month;
+    	cout.width(18); cout << "Year: "; cin >> record[k].year;
+    	cout.width(18); cout << "YearOfAdmission: "; cin >> record[k].yearOfAdmission;
+    	cout.width(18); cout << "Faculty: "; cin >> record[k].faculty;
+    	cout.width(18); cout << "Department: "; cin >> record[k].department;
+    	cout.width(18); cout << "Group: "; cin >> record[k].group;
+    	cout.width(18); cout << "Gender: "; cin >> record[k].gender;
+    	cout << "\n" << "Activity " << endl;
+    	for (int i = 1; i < 11; i++) {
+    		cout.width(22); cout << "Subject_and_mark" << i << ": "; cin >> record[k].activity.subject[i].value; cin >> record[k].activity.mark[i].value;
+    	}
+    }
+
+    void get_data(int k)
+    {
+    	cout << "\n" << "Student " << k << endl;
+    	cout.width(24); cout << "Surname: " << record[k].surname << endl;
+    	cout.width(24); cout << "Name: " << record[k].name << endl;
+    	cout.width(24); cout << "Patronymic: " << record[k].patronymic << endl;
+    	cout.width(24); cout << "Date: " << record[k].date << endl;
+    	cout.width(24); cout << "Month: " << record[k].month << endl;
+    	cout.width(24); cout << "Year: " << record[k].year << endl;
+    	cout.width(24); cout << "YearOfAdmission: " << record[k].yearOfAdmission << endl;
+    	cout.width(24); cout << "Faculty: " << record[k].faculty << endl;
+    	cout.width(24); cout << "Department: " << record[k].department << endl;
+    	cout.width(24); cout << "Group: " << record[k].group << endl;
+    	cout.width(24); cout << "Gender: " << record[k].gender << endl;
+    	cout << "\n" << "Activity " << endl;
+    	for (int i = 1; i < 11; i++) {
+    		cout.width(22); cout << record[k].activity.subject[i].value << " | " << record[k].activity.mark[i].value << endl;
+    	}
+    }
 };
 
 int main() {
+	Student student;
+	student.set_data(1);
+	cout << endl;
+	student.get_data(1);
 
 	return 0;
 }
